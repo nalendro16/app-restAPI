568ec734390e5ec18e397293fcee1a5314c0bd75
nalendro16 <nalendro_agung@yahoo.com>
2020-12-29 12:24:15 +0700

    menambah route tambah

--- controller.js
+++ controller.js
@@ -33,3 +33,22 @@ exports.tampilberdasaid = function (req, res) {
     }
   )
 }
+
+// menambah data
+exports.tambahmahasiswa = function (req, res) {
+  let nim = req.body.nim
+  let nama = req.body.nama
+  let jurusan = req.body.jurusan
+
+  connection.query(
+    'INSERT INTO mahasiswa (nim, nama, jurusan) VALUES(?,?,?)',
+    [nim, nama, jurusan],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error)
+      } else {
+        response.ok('Berhasil Menambahkan data', res)
+      }
+    }
+  )
+}
