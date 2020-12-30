80906572cd5d3aaf652b5ef5799f0a14452a973e
nalendro16 <nalendro_agung@yahoo.com>
2020-12-31 02:14:28 +0700

    berhasil membuat dan menyimpang token utk login

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,7 +13,7 @@ exports.registrasi = function (req, res) {
     //membuat array yg menerima data user
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: md5(req.body.password),
     role: req.body.role,
     tanggal_daftar: new Date(),
   }
@@ -48,10 +48,52 @@ exports.registrasi = function (req, res) {
 }
 
 // controller utk login
-exports.login(function (req, res) {
+exports.login = function (req, res) {
   let post = {
     //membuat array yg menerima data user
     password: req.body.password,
     email: req.body.email,
   }
-})
+
+  let query = 'SELECT * FROM ?? WHERE ??=? AND ??=?'
+  let table = ['user', 'password', md5(post.password), 'email', post.email]
+
+  query = mysql.format(query, table)
+  connection.query(query, function (error, rows) {
+    if (error) {
+      console.log(error)
+    } else {
+      if (rows.length == 1) {
+        let token = jwt.sign({ rows }, config.secret, { expiresIn: 1440 })
+        id_user = rows[0].id_user
+
+        // membuat token saat kondsi diatas terpenuhi
+        let data = {
+          id_user: id_user,
+          no_token: token,
+          ip_addres: ip.address(),
+        }
+
+        // menampung data ke akses token
+        let query = 'INSERT INTO ?? SET ?'
+        let table = ['akses_token']
+
+        query = mysql.format(query, table)
+        connection.query(query, data, function (error, rows) {
+          if (error) {
+            console.log(error)
+          } else {
+            res.json({
+              success: true,
+              massage: 'Token tergenerate',
+              token: token,
+              currUser: data.id_user,
+            })
+          }
+        })
+      } else {
+        res.json({ Error: true, Massage: 'Email/Password salah' })
+      }
+    }
+  })
+}
--- middleware/index.js
+++ middleware/index.js
@@ -2,7 +2,8 @@ let express = require('express')
 let auth = require('./auth')
 let router = express.Router()
 
-// mendaftarkan controller u/ menu register
+// mendaftarkan controller u/ menu register dan login
 router.post('/api/v1/register', auth.registrasi)
+router.post('/api/v1/login', auth.login)
 
 module.exports = router
