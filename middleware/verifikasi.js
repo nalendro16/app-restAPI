f35aebd535661f7dbe240d528a225f143f1c7c75
nalendro16 <nalendro_agung@yahoo.com>
2021-01-03 18:51:38 +0700

    membuat verifikasi token login - error solved

--- middleware/verifikasi.js
+++ middleware/verifikasi.js
@@ -3,32 +3,32 @@ const config = require('../config/secret')
 
 // agar user akses sesuai role
 function verifikasi() {
-  return function (req, res, next) {
+  return function (req, rest, next) {
     let role = req.body.role
     // cek auth token di header
     let tokenWithBearer = req.headers.authorization
-
+    // verifikasi token
     if (tokenWithBearer) {
-      let token = tokenWithBearer.split('')[1]
+      let token = tokenWithBearer.split(' ')[1]
       //   verifikasi
       jwt.verify(token, config.secret, function (err, decoded) {
         if (err) {
-          return res
+          return rest
             .status(401)
             .send({ auth: false, massage: 'token tidak terdaftar' })
         } else {
-          if (roles == 2) {
+          if (role == 2) {
             req.auth = decoded
-            next
+            next()
           } else {
-            return res
+            return rest
               .status(401)
               .send({ auth: false, massage: 'gagal autorisasi role anda' })
           }
         }
       })
     } else {
-      return res.status(401).send({ auth: false, massage: 'token tidak ada' })
+      return rest.status(401).send({ auth: false, massage: 'token tidak ada' })
     }
   }
 }
